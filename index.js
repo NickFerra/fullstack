82ad562d49b1841b9ba2d2b53f977618e2f20c90
Nicholas Ferrari <Nick.ferrari@me.com>
2021-03-08 12:24:06 +0000

    initial

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.write(JSON.stringify({"status": "Working"}));
+    res.end();
+});
+
+app.listen(5000, () => {
+    console.log("Online");
+});
